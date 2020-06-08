a3c3cbe944ba31560aaa14e5aaab0522653e31c5
Rolando Gonzales <rolandogo@gmail.com>
2020-06-08 14:58:26 +0000

    pruebas de aceptacion

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require './config'
+require './lib/anfitrion'
 
 get '/' do
-    'hola mundo'
+    erb  :hola
+end
+
+post '/saluda' do
+    anfitrion = Anfitrion.new params['idioma']
+    @mensaje = anfitrion.saluda params['nombre']
+    #@mensaje = params['idioma']
+    erb  :saluda
 end
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,1 +1,22 @@
 
+Given("Inicio la aplicacion del anfitrion") do
+    visit '/'
+  end
+  
+  When("Ingreso {string} como nombre") do |string|
+    fill_in("nombre", :with => string)
+  end
+  
+  When("Hago clic en Bienvenido") do
+    click_button("Bienvenido")
+  end
+
+  When("Selecciono {string} en el idioma") do |string|
+    select(string,:from => "idioma")
+  end
+  
+  Then("Debe decirme {string}") do |string|
+    expect(page.body).to match /#{string}/m
+  end
+
+  
