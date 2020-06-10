590d07bdad8e28b25fcb529109215b6b53233c78
Rolando Gonzales <rolandogo@gmail.com>
2020-06-10 16:41:58 +0000

    primer commit ahorcado

--- app.rb
+++ app.rb
@@ -1,14 +1,10 @@
 require 'sinatra'
 require './config'
-require './lib/anfitrion'
 
 get '/' do
     erb  :hola
 end
 
-post '/saluda' do
-    anfitrion = Anfitrion.new params['idioma']
-    @mensaje = anfitrion.saluda params['nombre']
-    #@mensaje = params['idioma']
-    erb  :saluda
+post '/' do
+    'Salvaste tu vida'
 end
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,22 +1,13 @@
 
-Given("Inicio la aplicacion del anfitrion") do
-    visit '/'
-  end
-  
-  When("Ingreso {string} como nombre") do |string|
-    fill_in("nombre", :with => string)
-  end
-  
-  When("Hago clic en Bienvenido") do
-    click_button("Bienvenido")
-  end
+Given("Inicio el juego con palabra secreta iniciada") do
+  visit '/'
+end
 
-  When("Selecciono {string} en el idioma") do |string|
-    select(string,:from => "idioma")
-  end
-  
-  Then("Debe decirme {string}") do |string|
-    expect(page.body).to match /#{string}/m
-  end
+When("Ingreso {string} como palabra a adivinar") do |string|
+  fill_in("palabra", :with => string)
+  click_button("Adivinar")
+end
 
-  
+Then("Debe decirme {string}") do |string|
+  expect(page.body).to match /#{string}/m
+end
